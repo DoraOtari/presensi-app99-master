73803f42b58ffb5d0536c85d79cee387afe8a80e
doraotari <doraotari@gmail.com>
2023-11-03 20:16:17 +0700

    p15 kota

--- app/Http/Livewire/Karyawan/Create.php
+++ app/Http/Livewire/Karyawan/Create.php
@@ -4,11 +4,12 @@ namespace App\Http\Livewire\Karyawan;
 
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class Create extends Component
 {
-    public  $tgl_lahir, $user_id;
+    public  $tgl_lahir, $user_id, $provinsi_id;
 
     public function buatNik()
     {
@@ -18,12 +19,26 @@ class Create extends Component
 
     }
 
+    public function provinsi()
+    {
+        $prov = Http::get('https://emsifa.github.io/api-wilayah-indonesia/api/provinces.json');
+        return  $prov->collect();
+    }
+
+    public function kota()
+    {
+        $id = explode("/",$this->provinsi_id)[0];
+        return Http::get('https://emsifa.github.io/api-wilayah-indonesia/api/regencies/'.$id.'.json')->collect();
+    }
+
     public function render()
     {
         return view('livewire.karyawan.create', [
-            'jabatan' => DB::table('jabatan')->get(),
-            'user'    => User::all(),
-            'nik'     => $this->buatNik(),
+            'jabatan'  => DB::table('jabatan')->get(),
+            'user'     => User::all(),
+            'nik'      => $this->buatNik(),
+            'provinsi' => $this->provinsi(),
+            'kota'     => $this->kota(),
             ])->layout('layouts.template');
     }
 }
--- resources/views/livewire/karyawan/create.blade.php
+++ resources/views/livewire/karyawan/create.blade.php
@@ -50,6 +50,30 @@
                         @endforeach
                     </select>
                 </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="provinsi" class="form-label">Provinsi</label>
+                            <select wire:model='provinsi_id' class="form-select " name="provinsi" id="provinsi">
+                                <option value="null" disabled selected>--Pilih Satu--</option>
+                                @foreach ($provinsi as $item)
+                                    <option value="{{ $item['id'].'/'.$item['name'] }}">{{ $item['name'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="kota" class="form-label">Kota</label>
+                            <select class="form-select " name="kota" id="kota">
+                                <option value="null" disabled selected>--Pilih Satu--</option>
+                                @foreach ($kota as $item)
+                                <option value="{{ $item['id'].'/'.$item['name'] }}">{{ $item['name'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
